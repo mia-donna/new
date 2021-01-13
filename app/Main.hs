a417d8fac5800a907c929a3e7fe8d65dcfc6f954
Mia <mia@vidops.io>
2021-01-13 21:59:07 +0000

    create customers, put them into a list

--- app/Main.hs
+++ app/Main.hs
@@ -27,8 +27,29 @@ type Account = Int
 type Balance =  Int
 type Name = String
 type Value = Int
+type List = [MVar Type]
 
 data Type = Payee | Recipient deriving (Show, Eq)
+
+randomN :: IO Int 
+randomN = do
+    r <- randomRIO (1, 4)
+    return r
+
+-- CUSTOMER THREAD PROCESS : picks random type, puts the type in a type box and customer in a customer box
+process :: Customer -> MVar Customer -> MVar Value -> MVar Type -> IO () 
+process cust custbox value typebox = do
+    --v <- takeMVar value -- just a method to run it / blocks it if enabled
+    t1 <- typeFlip
+    putMVar custbox cust
+    putStrLn $ (show cust) ++ " -- got " ++ (show t1)
+    if t1 == Payee then do
+        putStrLn $ (show cust) ++ " -- joining Payee group"
+        --putMVar typebox Payee
+        
+    else do 
+       putStrLn $ (show cust) ++ " -- joining Recipient group"   
+       --putMVar typebox Recipient
 
 typeFlip :: IO Type
 typeFlip = do
@@ -50,54 +71,68 @@ main = do
     three <- newEmptyMVar
     four <- newEmptyMVar
     value <- newEmptyMVar -- for value
-    --typebox1 <- newEmptyMVar -- for random customer type
-    --typebox2 <- newEmptyMVar -- for random customer type
-    --typebox3 <- newEmptyMVar -- for random customer type
-    --typebox4 <- newEmptyMVar -- for random customer type
-    typebox <- newEmptyMVar -- for random customer type
-    list <- newEmptyMVar -- for bool list
+    -- list <- newEmptyMVar -- attempting list of customers ** so i think customer list is created in main
+    typebox1 <- newEmptyMVar -- for random customer type
+    typebox2 <- newEmptyMVar -- for random customer type
+    typebox3 <- newEmptyMVar -- for random customer type
+    typebox4 <- newEmptyMVar -- for random customer type
+    -- typebox <- newEmptyMVar -- for random customer type
     -- fork the customer processes
     putStrLn $ "4 customer threads being created."
-    mapM_ forkIO [process c1 one value typebox , process c2 two value typebox , process c3 three value typebox, process c4 four value typebox ] 
+    mapM_ forkIO [process c1 one value typebox1 , process c2 two value typebox2 , process c3 three value typebox3 , process c4 four value typebox4 ] 
     -- create a box for each process
     c <- takeMVar one
     d <- takeMVar two
     e <- takeMVar three
     f <- takeMVar four
-    putStrLn $ "hi " ++ (show c)
-    putStrLn $ "hi " ++ (show d)
-    putStrLn $ "hi " ++ (show e)
-    putStrLn $ "hi " ++ (show f)
+    let list_try = c:d:e:f:[]
+    putStrLn $ "HIhere!!!!" ++ (show list_try)
+    putStrLn $ "here!!!!" ++ (show (list_try!!0)) 
+    --putStrLn $ "hi " ++ (show c)
+    --putStrLn $ "hi " ++ (show d)
+    --putStrLn $ "hi " ++ (show e)
+    --putStrLn $ "hi " ++ (show f)
     --test <- takeMVar typebox1 -- only runs if c1 gets Payee
-    test <- takeMVar typebox  
 
-    putStrLn $ "type test " ++ (show test)
-    putStrLn $ "hi " ++ (show c) 
+    -- a- uncomment + use this is check the TYPE of the thread: test <- takeMVar typebox  
+    --  b- uncomment + use this is check the TYPE of the thread: putStrLn $ "type test " ++ (show test)
+    
+    
+
+    -- Can work with a pair of values in MVar box
+    --list <- newEmptyMVar
+    --test1 <- takeMVar typebox1
+    --test2 <- takeMVar typebox2
+    
+     --test mvars in samebox
+    --list2 <- newEmptyMVar
+    --putMVar list2 c
+   -- putMVar list2 d 
+   -- putMVar list2 e
+   -- putMVar list2 f
+    --putStrLn $ "customers added to a list"
+   -- h <- takeMVar list2
+    putStrLn $ "hi" 
+
+    --putMVar list (test1,test2)
+    --pair <- takeMVar list 
+    --let testa = fst pair
+    --let testb = snd pair
+    --if testa == testb then
+        --putStrLn $ "shit - they're the same!"
+     --else
+        --putStrLn $ "woop ti doop - we can start the transfer!"   
+    --list_b <- newEmptyMVar  
+    --putMVar h addtypetolist
+
+ -- print all threads (test)
+    {-putStrLn $ "hi " ++ (show c) 
     putStrLn $ "hi " ++ (show d) 
     putStrLn $ "hi " ++ (show e) 
-    putStrLn $ "hi " ++ (show f) 
-    
-    putStrLn $ "done"
-
--- CUSTOMER THREAD PROCESS : picks random type, puts the type in a type box and customer in a customer box
-process :: Customer -> MVar Customer -> MVar Value -> MVar Type -> IO () 
-process cust custbox value typebox = do
-    --v <- takeMVar value -- just a method to run it / blocks it if enabled
-    t1 <- typeFlip
-    putMVar custbox cust
-    putStrLn $ (show cust) ++ " -- got " ++ (show t1)
-    if t1 == Payee then do
-        putStrLn $ (show cust) ++ " -- joining Payee group"
-        putMVar typebox Payee
-    else do 
-       putStrLn $ (show cust) ++ " -- joining Recipient group"   
-       putMVar typebox Recipient
+    putStrLn $ "hi " ++ (show f) -}
 
 
-randomN :: IO Int 
-randomN = do
-    r <- randomRIO (1, 4)
-    return r
+
 
 
 
@@ -113,7 +148,7 @@ randomN = do
 -- ACTIONS IN MAIN THREAD
 -- 1. Create 10 customers
 -- 2. Create 10 customer processes/threads
--- 3. Pick two customer MVars (True, False)
+-- 3. Create an Mvar List customers (one for Payee, one for Recipient) and pick two randomly
 -- So need to add the customer MVars into a list of MVars of type Bool, then use a random selector to pick them
 --  
 
@@ -123,5 +158,22 @@ randomN = do
 -- If False == then payee
 -- first one to both is the match
 -- need to put their result in a box
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
